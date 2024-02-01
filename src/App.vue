1fb089ed95ecc4ee11cf408b847f6170990fe25c
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-01 13:26:38 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppMain from './components/AppMain.vue';
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppMain,
+  }
+}
+
+
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as*;
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,19 @@
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+        
+    }
+}
+</script>
+
+
+<template>
+</template>
+
+<script scoped></script>
