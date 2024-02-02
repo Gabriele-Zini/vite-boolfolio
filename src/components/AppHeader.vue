174019dbf8dcb2eceaddc3b2bcf717094ae634df
Gabriele-Zini <139758244+Gabriele-Zini@users.noreply.github.com>
2024-02-02 18:04:27 +0100

    add: header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                {
+                    name:'home',
+                    title:'Home',
+                },
+                {
+                    name:'about',
+                    title:'About',
+                },
+               
+            ]
+        }
+
+    }
+}
+</script>
+
+<template>
+    <header class="d-flex align-items-center justify-content-end">
+        <div class="container">
+            <nav class="nav">
+                <router-link class="nav-link" v-for="link in links" :to="{ name: link.name }">{{ link.title }}</router-link>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    min-height: 100px;
+    background-color: rgb(35, 31, 31);
+}
+</style>
